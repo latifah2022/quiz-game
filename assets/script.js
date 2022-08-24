c7c40482b6e724202e93e891cbbf8c8fe2e217ee
Latifah Zawedde <zawedde2021@gmail.com>
2022-08-23 20:31:37 -0700

    8-commit

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,15 @@ var startButton = document.getElementById('start-btn')
 var questionContainerEl = document.getElementById('questions-container')
 var timerCountEl = document.querySelector(".timer-count")
 var question = document.getElementById("questions");
+var containerEl = document.querySelector(".container")
+var scoresEl = document.querySelector(".scores")
 var btnList = document.querySelectorAll(".btn");
+var scoreList = document.querySelector(".score")
+var highscoremenue = document.querySelector(".highscoremenue")
 console.log(btnList);
 var currentQuestion = 0;
-var timerCount = 50
+var timerCount = 50;
+var lastQuestion = 4;
 
 //start the game(click start)
 function startGame() {
@@ -17,15 +22,24 @@ function startGame() {
     setNextQuestion();
 }
 
+// function endGame() {
+//     console.log("ended")
+
+// }
+
 //set next question (click next)
 function setNextQuestion() {
+if (currentQuestion < lastQuestion){
+
     // get the current question from myQuestions
     var questionObject = myquestions[currentQuestion];
     //indicates the correct answer
     var correctAnswer = myquestions[currentQuestion]["correct"];
 
     //change the question to questionObject
-    question.innerText = questionObject.question;
+    question.innerHTML = myquestions[currentQuestion].question;
+    console.log(question)
+    console.log(questionObject)
     
     // 
     btnList.forEach(function(btnElement, index) {
@@ -37,14 +51,14 @@ function setNextQuestion() {
         })
     })
 
-    //  if(currentQuestion === 0){
-         
-    //     }
-
+} else {
+    console.log("nextquestion")
+}
+    console
     // function nextQuestion() {
     //     console.log("next")
-    //    var questionObject = myquestions[currentQuestion, i++]
-    //    for (var i = 0; i < currentQuestion.length; i++);
+    // var questionObject = myquestions[currentQuestion, i++]
+    //  for (var i = 0; i < currentQuestion.length; i++);
     // }
 }
 
@@ -56,6 +70,11 @@ function selectAnswer(correctAnswerIndex, event) {
     var idValue = target.getAttribute("id");
     if(idValue == correctAnswerIndex) {
         console.log("Correct answer");
+        currentQuestion++;
+        setNextQuestion();
+    } else {
+        timerCount = timerCount - 10
+        currentQuestion++; setNextQuestion ()
     }
     //inccorrect answer deduct 5sec from timercount.
 }
@@ -66,11 +85,17 @@ function timer() {
     var timer = setInterval(function () {
         timerCount--;
         timerCountEl.textContent = timerCount;
-            if (timerCount === 0){
+            if (timerCount <= 0){
                 clearInterval(timer);
                 //endGame()
             }
     }, 1000)
+}
+
+function viewscreen () {
+    console.log("scores")
+    highscoremenue.setAttribute("style", "display:block")
+    containerEl.setAttribute("style", "display:none")
 }
 //questions
 var myquestions =  [
@@ -90,17 +115,29 @@ var myquestions =  [
         correct: 1
     },
     {
-         question: "What planet are we on?",
-         answer: [ "Mars", "Jupiter", "America", "Earth"],
-         correct: 3
+        question: "What planet are we on?",
+        answer: [ "Mars", "Jupiter", "America", "Earth"],
+        correct: 3
     },
     {
-         question: "What is the best programming language?",
-         answer: [ "Python", "Java", "Javascript", "Css"],
+        question: "What is the best programming language?",
+        answer: [ "Python", "Java", "Javascript", "Css"],
         correct: 2 
     }
 ]
 
+function scoreList() {
+    initialsScore.textContent = "";
+    scoreList.textContent = "";
+    for (var i = 0; i < currentQuestion.length; i++);
+         var initials = initialsScore[i]
+         var score = scoreList[i];
+
+
+
+}
+
 
 
 startButton.addEventListener('click', startGame)
+scoresEl.addEventListener("click", viewscreen)
